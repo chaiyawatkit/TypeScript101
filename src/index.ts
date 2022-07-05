9a39273830370c42dde56d67fda3fc0054f185c4
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 00:00:43 +0700

    type Assertions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-const formatFloat = (num: string | number) => {
-  switch (typeof num) {
-    case 'string':
-      return parseFloat(num).toFixed(2);
-    case 'number':
-      return num.toFixed(2);
-    default:
-      return num;
-  }
-};
+let num: unknown;
+
+num = 20;
+
+(<number>num).toFixed(2);
