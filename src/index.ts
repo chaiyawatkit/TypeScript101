09da3fff07e434b525f005af6d84c53151ebd914
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 00:13:38 +0700

    tuples

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-const num1 = [1, 2, 3];
-const num2: unknown[] = ['hello', 2, false];
-const num3: Array<number> = [4, 5, 6];
+const point: [x: number, y: number] = [10, 20]; //x= 10 ,y =20
 
-num3[0] = 1;
-const num4: readonly number[] = [1, 2, 3];
-// num4[0] = 10;
-
-const num5: ReadonlyArray<number> = [4, 5, 6];
-// num5[0] = 10;
-
-const num6 = num5 as number[];
-const num7 = <number[]>num5;
+point.push(30);
+console.log(point);
+console.log(point[1]);
