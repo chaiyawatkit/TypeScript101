f870ae75cc37e7ac785e38f090234fff77835bb7
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 00:33:56 +0700

    Readonly and Optional Properties

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
-interface Website {
-  url: string;
+interface Person {
+  fName: string;
+  lName: string;
+  middleName?: string;
+  readonly gender: 'Male' | 'Female';
 }
-interface Article {
-  title: string;
-  content: string;
-}
-interface BlogPost extends Website, Article {
-  view: number;
-}
-const post: BlogPost = {
-  url: 'www.google.com',
-  title: 'extent interface',
-  content: '...',
-  view: 999,
+
+let kuk: Person = {
+  fName: 'kuk',
+  lName: 'kik',
+  gender: 'Male',
 };
-console.log(post);
