b1667395d3bf45fe07d258cf8baccfc68a830499
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-05 23:55:22 +0700

    void

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-const fixed = (num: number) => {
-  return num.toFixed(2);
-};
-
-let num: unknown = '10.511119';
-num = 10.511119;
-num = false;
-
-if (typeof num === 'number') {
-  console.log(fixed(num));
-}
+const greet = () => console.log('no return');
+greet();
