bbabff0bb2465c47f9e08f05b3b1305df8f559c7
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 00:08:15 +0700

    array

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-let num: unknown;
+const num1 = [1, 2, 3];
+const num2: unknown[] = ['hello', 2, false];
+const num3: Array<number> = [4, 5, 6];
 
-num = 20;
+num3[0] = 1;
+const num4: readonly number[] = [1, 2, 3];
+// num4[0] = 10;
 
-(<number>num).toFixed(2);
+const num5: ReadonlyArray<number> = [4, 5, 6];
+// num5[0] = 10;
+
+const num6 = num5 as number[];
+const num7 = <number[]>num5;
