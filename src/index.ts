4ee850f873c916e8a1f5bf86d973dcb713f1c834
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-05 23:57:39 +0700

    never

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-const greet = () => console.log('no return');
-greet();
+const formatFloat = (num: string | number) => {
+  switch (typeof num) {
+    case 'string':
+      return parseFloat(num).toFixed(2);
+    case 'number':
+      return num.toFixed(2);
+    default:
+      return num;
+  }
+};
