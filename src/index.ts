267f6d1e338208576529ecdbe44b91c6bf51c8af
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 12:06:36 +0700

    Mapped Types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
-interface States {
-  [states: string]: boolean;
+interface Course {
+  title: string;
+  credit: number;
 }
-const states: States = { enabled: true, readable: true };
+
+type CourseReadOnly = {
+  readonly [K in keyof Course]: Course[K];
+};
+
+type CourseReadOnlyWithSemaster = CourseReadOnly & { semaster: string };
+
+type CourseOptional = {
+  [K in keyof Course]?: Course[K];
+};
+
+type CourseReadOnlyReauired = {
+  readonly [K in keyof CourseOptional]-?: CourseOptional[K];
+};
