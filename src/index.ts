13046d100eff3762fb930cd89aefb77526a3a68b
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 13:38:45 +0700

    Generic Types กับ Type Aliases และ Interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
-const last = <T>(arr: T[], count: number) => {
-  return arr.slice(arr.length - count);
-};
-last<number>([1, 2, 3, 4, 5], 3); //[3,4,5]
-
-last<string>(['A', 'B', 'C', 'D', 'E'], 2); //[D,E]
-
-class Stack<T> {
-  private items: T[] = [];
-
-  push(items: T) {
-    this.items.push(items);
-  }
-  pop(): T | undefined {
-    return this.items.pop();
-  }
+interface Dict<T> {
+  [key: string]: T;
 }
 
-const s = new Stack<number>();
-s.push(1);
-s.push(2);
-s.push(3);
+// type Dict<T> = {
+//   [key: string]: T;
+// }
+const initialize = <T>(keys: string[], value: T): Dict<T> => {
+  return keys.reduce(
+    (result, key) => ({
+      ...result,
+      [key]: value,
+    }),
+    {}
+  );
+};
 
-s.pop();
+const result = initialize(['a', 'b', 'c'], 0);
 
-console.log(s);
+console.log(result);
