826e31e4ac56677911ad0eb324074710cda788e7
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 10:45:29 +0700

    Function Overloading

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-// function getFullName(fname: string, lname: string) {
-//   return `${fname} ${lname}`;
-// }
+function addInt(n: number, m: number): number;
+function addInt(n: string, m: string): string;
+function addInt(n: unknown, m: unknown): unknown {
+  if (typeof n === 'number' && typeof m === 'number') {
+    return 1;
+  }
+  if (typeof n === 'string' && typeof m === 'string') {
+    return 'string 1';
+  }
+}
 
-// const getFullName = function (fname: string, lname: string) {
-//   return `${fname} ${lname}`;
-// };
-const getFullName = (fname: string, lname: string) => {
-  return `${fname} ${lname}`;
-};
+console.log(addInt(1, 2));
+
+console.log(addInt('1', '2'));
