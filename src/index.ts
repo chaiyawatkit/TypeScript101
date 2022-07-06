d1acdb151eac4869c030e5ee9498a36b683114e2
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 11:59:50 +0700

    Indexable Types

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,4 @@
-interface Person {
-  name: string;
-  age: number;
-  address: string;
+interface States {
+  [states: string]: boolean;
 }
-type PersonKeys = keyof Person;
-type PersonAgeType = Person['age'];
-type PersonValues = Person[keyof Person];
-
-let test: Person = {
-  name: 'kuk',
-  age: 2,
-  address: '192/142 bkk',
-};
-
-const printValue = (key: keyof Person) => {
-  console.log(test[key]);
-};
-
-printValue('name');
+const states: States = { enabled: true, readable: true };
