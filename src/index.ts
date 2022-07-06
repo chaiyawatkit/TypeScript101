6b71e593d422b4d158649e4acc2b578ac07458c7
kukmama <chaiwatkit.tc.4.2@gmail.com>
2022-07-06 12:51:57 +0700

    class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-type Person = {
+class Person {
   name: string;
   age: number;
-};
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  printDetile() {
+    console.log('name', this.name, 'age', this.age);
+  }
+}
 
-type PersonSub = {
-  [K in keyof Person as `${K}Changed`]: (key: K, value: Person[K]) => void;
-};
+let somchai = new Person('somchai', 24);
+somchai.age;
+somchai.name;
+somchai.printDetile();
+let somsree = new Person('somsree', 18);
+somsree.printDetile();
